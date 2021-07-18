d35d8212b24debe92fc0cf280da03e3460b0d4c1
Yordy <kevinmacmasel@gmail.com>
2021-07-18 14:31:18 +0700

    first commit

---
+++ app/src/main/java/com/example/smarttumbler/statistic/StatisticFragment.java
@@ -0,0 +1,36 @@
+package com.example.smarttumbler.statistic;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smarttumbler.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+public class StatisticFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View root = inflater.inflate(R.layout.fragment_statistic, container, false);
+        GraphView graph = root.findViewById(R.id.graph);
+
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        return root;
+    }
+}
